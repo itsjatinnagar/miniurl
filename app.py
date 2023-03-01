c00ca68ed9ea25e6b85aef779260893f792f4eab
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2023-03-01 21:22:59 +0530

    Define Route for OTP-based Login

--- app.py
+++ app.py
@@ -1,7 +1,52 @@
-from flask import Flask, render_template
+from controllers.mailer import emailCode
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, session, redirect, url_for
+import os
+import random
+import string
+
+OTP_LENGTH = 6
+load_dotenv()
+
 
 app = Flask(__name__)
+app.secret_key = os.environ['SESSION_KEY']
+
 
 @app.route("/")
 def index():
-    return render_template('index.html', username = 'nagarjatin336')
+    try:
+        identifiers = session['userID']
+    except:
+        identifiers = None
+
+    if identifiers is None:
+        return render_template('index.html')
+    else:
+        return render_template('index.html', username = 'nagarjatin336')
+
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        code = ''.join(random.choices(string.digits, k=OTP_LENGTH))
+        isSent = emailCode(request.args['email'], code)
+        if isSent:
+            session['user_email'] = request.args['email']
+            session['auth_code'] = code
+            return "Success", 200
+        else:
+            return "Unsuccessful", 500
+    else:
+        if session['auth_code'] == request.json['code']:
+            session.pop('user_email')
+            session.pop('auth_code')
+            session['userID'] = '1'
+            return "Success", 200
+        else:
+            return "Invalid Code", 401
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
---
+++ controllers/mailer.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+from email.message import EmailMessage
+import os
+import logging
+import smtplib
+
+
+load_dotenv()
+
+
+def emailCode(userEmail, code):
+    email = os.environ['EMAIL_ADDR']
+    password = os.environ['EMAIL_PASS']
+    message = EmailMessage()
+    message['From'] = email
+    message['To'] = userEmail
+    message['Subject'] = 'Login Code for MiniUrl'
+    # Fallback Content
+    message.set_content(
+        f"Hey {userEmail.split('@')[0]}!\n\nYour sign in code is: {code}\n\nThanks,\nMiniUrl")
+    # HTML Content
+    message.add_alternative(f"""\
+    <!DOCTYPE html>
+    <html lang="en">
+    <body>
+        <p style="font-size:18px;font-weight:bold;">Hey {userEmail.split('@')[0]}!</p>
+        <p style="font-size:20px;text-align:center;">Your sign in code is: <strong style="color:#2acfcf;">{code}</strong></h2>
+        <p>Thanks,<br>MiniUrl</p>
+    </body>
+    </html>
+    """, subtype='html')
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        try:
+            smtp.login(email, password)
+            smtp.send_message(message)
+        except smtplib.SMTPException as error:
+            logging.error(error)
+            return False
+    return True
