e80e5e961f49439405312192bf708278b514778b
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2023-04-06 17:33:08 +0530

    Hamburger Menu for Small Screens

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,32 @@
 function showPopup() {
     console.log("Popup to show error message is appeared");
 }
+
+const menuOpenAction = () => {
+    menu.classList.add("active");
+    menuButton.dataset.action = "close";
+};
+
+const menuCloseAction = () => {
+    menu.classList.remove("active");
+    menuButton.dataset.action = "open";
+};
+
+const menuButton = document.getElementById("btn-menu");
+const menu = document.getElementById("menu");
+
+menuButton.onclick = (event) => {
+    switch (event.target.dataset.action) {
+        case "open":
+            menuOpenAction();
+            break;
+
+        case "close":
+            menuCloseAction();
+            break;
+
+        default:
+            console.error("Invalid menuButton action");
+            break;
+    }
+};
