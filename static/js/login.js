7f297f6b8e33d4696d32ef0f2578045feefec471
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2023-04-06 22:08:05 +0530

    Implement Login

--- static/js/login.js
+++ static/js/login.js
@@ -1,36 +1,69 @@
-const modalOverlay = document.getElementById("overlay");
-const modal = document.getElementById("modal");
-const sendCodeButton = document.getElementById("btn-sendCode");
-const verifyButton = document.getElementById("btn-verify");
+const openLoginModal = () =>
+    document.body.classList.add(...["modal-active", "login-modal"]);
 
-sendCodeButton.addEventListener("click", () => {
-    const userEmail = document.getElementById("email").value;
-    fetch(`/login?email=${userEmail}`).then((response) =>
-        response.status == 200 ? openModal() : showPopup()
-    );
+const closeLoginModal = () =>
+    document.body.classList.remove(...["modal-active", "login-modal"]);
+
+const loginButtons = document.querySelectorAll("button.btn-get-started");
+loginButtons.forEach((button) => (button.onclick = openLoginModal));
+
+const loginOverlay = document.getElementById("overlay");
+loginOverlay.onclick = closeLoginModal;
+
+const sendCodeButton = document.getElementById("btn-send-code"),
+    emailInput = document.getElementById("email");
+
+sendCodeButton.addEventListener("click", async () => {
+    const response = await request(`/login?email=${emailInput.value}`, "GET");
+    if (response) document.getElementById("modal").classList.add("code-sent");
 });
 
-verifyButton.addEventListener("click", () => {
-    const code = document.getElementById("otp").value;
-    fetch("/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ code: code }),
-    }).then((response) =>
-        response.status == 200 ? window.location.reload() : showPopup()
-    );
+const submitButton = document.getElementById("btn-submit"),
+    otpInputs = document.querySelectorAll("#otp-input input");
+
+otpInputs.forEach((input, index) => {
+    input.addEventListener("keyup", (event) => {
+        const currentInput = input,
+            nextInput = input.nextElementSibling,
+            prevInput = input.previousElementSibling;
+
+        if (currentInput.value.length > 1) {
+            currentInput.value = "";
+            return;
+        }
+
+        if (
+            nextInput &&
+            nextInput.hasAttribute("disabled") &&
+            currentInput.value !== ""
+        ) {
+            nextInput.removeAttribute("disabled");
+            nextInput.focus();
+        }
+
+        if (event.key === "Backspace") {
+            otpInputs.forEach((input, innerIndex) => {
+                if (index <= innerIndex && prevInput) {
+                    input.setAttribute("disabled", true);
+                    currentInput.value = "";
+                    prevInput.focus();
+                }
+            });
+        }
+    });
 });
 
-function openModal() {
-    document.body.classList.add("overlay-active");
-    modalOverlay.classList.add("active");
-    modal.classList.add("active");
-}
+submitButton.addEventListener("click", async () => {
+    let code = "";
+    otpInputs.forEach((input) => (code += input.value));
+    const response = await request(
+        "/login",
+        "POST",
+        { "Content-Type": "application/json" },
+        JSON.stringify({ code: code })
+    );
 
-modalOverlay.addEventListener("click", () => {
-    document.body.classList.remove("overlay-active");
-    modalOverlay.classList.remove("active");
-    modal.classList.remove("active");
+    if (response) {
+        window.location.reload();
+    }
 });
--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,25 @@
+const request = (uri, method, headers, body) =>
+    new Promise(async (resolved) => {
+        const response = await fetch(uri, {
+            method: method,
+            headers: headers,
+            body: body,
+        });
+        if (response.status === 200) resolved(true);
+        else if (response.status === 500) showPopup();
+        else resolved(false, response.status, response.body());
+    });
+
 function showPopup() {
     console.log("Popup to show error message is appeared");
 }
 
-const menuOpenAction = () => {
+const openMenu = () => {
     menu.classList.add("active");
     menuButton.dataset.action = "close";
 };
 
-const menuCloseAction = () => {
+const closeMenu = () => {
     menu.classList.remove("active");
     menuButton.dataset.action = "open";
 };
@@ -18,11 +30,11 @@ const menu = document.getElementById("menu");
 menuButton.onclick = (event) => {
     switch (event.target.dataset.action) {
         case "open":
-            menuOpenAction();
+            openMenu();
             break;
 
         case "close":
-            menuCloseAction();
+            closeMenu();
             break;
 
         default:
