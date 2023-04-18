b3284031a6947fc91b9b5c723e37b85c5fcaceef
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2023-04-18 13:53:55 +0530

    Shorten Cards UI

--- static/js/shorten.js
+++ static/js/shorten.js
@@ -13,50 +13,22 @@ shortenButton.addEventListener("click", async () => {
     }
 });
 
-// shortenButton.addEventListener("click", () => {
-//     const url = document.getElementById("url").value;
-//     fetch("/shorten", {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ long_url: url }),
-//     }).then((response) =>
-//         response.status == 200 ? window.location.reload() : showPopup()
-//     );
-// });
+const dateSpans = document.querySelectorAll("span.date");
+dateSpans.forEach((span) => {
+    const datetimeUTC = new Date(span.textContent * 1000);
+    const datetime = new Date(
+        datetimeUTC.getFullYear(),
+        datetimeUTC.getMonth(),
+        datetimeUTC.getDate()
+    );
+    span.textContent = Intl.DateTimeFormat().format(datetime);
+});
 
-// linksButton.forEach((button) => {
-//     button.addEventListener("click", () => {
-//         fetch(`/get/${button.dataset.linkid}`)
-//             .then((response) =>
-//                 response.status == 200 ? response.text() : showPopup()
-//             )
-//             .then((data) => openSidebar(data));
-//     });
-// });
-
-// function openSidebar(HTML_CONTENT) {
-//     document.body.classList.add("overlay-active");
-//     sidebarOverlay.classList.add("active");
-//     sidebar.classList.add("active");
-//     sidebar.innerHTML = HTML_CONTENT;
-//     activateClipboard();
-// }
-
-// sidebarOverlay.addEventListener("click", () => {
-//     document.body.classList.remove("overlay-active");
-//     sidebarOverlay.classList.remove("active");
-//     sidebar.classList.remove("active");
-//     sidebar.innerHTML = "";
-// });
-
-// function activateClipboard() {
-//     const copyButton = document.getElementById("btn-copy");
-//     copyButton.addEventListener("click", () => {
-//         navigator.clipboard
-//             .writeText(sidebar.querySelector("p.short-link").textContent)
-//             .then((response) => console.log(response))
-//             .catch((error) => console.log(error));
-//     });
-// }
+const copyButtons = document.querySelectorAll("button.btn-copy");
+copyButtons.forEach((button) => {
+    button.addEventListener("click", () =>
+        navigator.clipboard
+            .writeText(button.dataset.link)
+            .catch((error) => console.log(error))
+    );
+});
