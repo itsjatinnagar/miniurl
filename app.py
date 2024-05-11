b62302b8d5f3ffbda653744bc9ff70e33311010e
Jatin Nagar <121741542+itsjatinnagar@users.noreply.github.com>
2024-05-11 09:27:00 +0530

    Release v3.1.2

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from routes.misc import misc_bp
 from routes.user import user_bp
 
 app = Flask(__name__, static_folder='app', static_url_path='/')
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=3)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 app.config.update(
